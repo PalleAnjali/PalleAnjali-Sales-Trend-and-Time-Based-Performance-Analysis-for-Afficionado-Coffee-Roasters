4d20125eec3d5eb386f3b6ec42273cf69c64d9f8
Palle Anjali <anjalianju5419@gmail.com>
2026-04-05 09:34:35 +0530

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.title("☕ Sales Trend and Time-Based Performance Analysis for Afficionado Co
 # ---------------------------
 # 1️⃣ Load Data
 # ---------------------------
-df = pd.read_csv(r"Afficionado Coffee Roasters")
+df = pd.read_csv(r"Afficionado Coffee Roasters.csv")
 
 def load_data():
     df['transaction_datetime'] = pd.to_datetime(df['transaction_datetime'])
